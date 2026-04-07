5c3890563f1b0ef8887b3aebf5a64faa3010cb0c
luq27090-bit <luq27090@gmail.com>
2026-04-08 04:21:31 +0800

    Update site settings for Odysell theme

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `LUQIU | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://LUQIU.sapling.supply`, // No trailing slash!
-	name: `LUQIU`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Odysell |  Used Construction Machinery, Cars, Mobile Phones and Apparel Businesses`,
+	description: `A Trading Website for Secondhand Construction Equipment, Vehicles, Phones and Clothing.`,
+	url: `https://Odysell.sapling.supply`, // No trailing slash!
+	name: `Odysell`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
