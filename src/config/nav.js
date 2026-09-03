c12bff5a80f2ea2603320fa9b2df5f67e3684d19
luq27090-bit <luq27090@gmail.com>
2026-09-04 01:16:29 +0800

    Update navigation for Second Life site

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,7 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
-	},
+	{ title: 'Home', slug: '/' },
+	{ title: 'Customize', slug: '/customize' },
+	{ title: 'Signature', slug: '/signature' },
+	{ title: 'Craft', slug: '/craft' },
+	{ title: 'About', slug: '/about' },
 ];
